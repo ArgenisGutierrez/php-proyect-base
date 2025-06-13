c667574ca6a72d83135c304f01514ec9cbdfa39b
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-06-12 22:21:34 -0600

    feat:uso de erro_logs
    se agrego para usar logs de errores para las clases core

--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '0');
+ini_set('log_errors', '1');
+ini_set('error_log', __DIR__ . '/../storage/logs/app.log');
+
 require __DIR__ . '/../config/config.php';
 require __DIR__ . '/../config/routes.php';
